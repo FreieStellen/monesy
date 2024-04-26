aa82dab8c14634948f8382a55d4f5e643f90359a
programmer <2625976518@qq.com>
2024-04-26 11:45:19 +0800

    只出现一次的数字

--- JAVA/src/位运算/SingleNumber.java
+++ JAVA/src/位运算/SingleNumber.java
@@ -4,7 +4,8 @@ package 位运算;
  *@ClassName SingleNumber 只出现一次的数字
  *@Author Freie  stellen
  *@Date 2024/4/24 11:18
- *思路：在限制时间复杂度和空间复杂度的情况下，可以利用异或位运算来排除数组中的重复元素
+ *思路：在限制时间复杂度和空间复杂度的情况下，可以利用异或位运算来排除数组中的重复元素，异或运算的本质是模2加法运算（对运算中所有的二进制数相加）
+ * 例如：13^13本质上是13的二进制数1101+1101=2202，再对二进制的每一位数对2取余得到结果
  * 时间复杂度：o（n）——————空间复杂度：o（1）
  */
 public class SingleNumber {
@@ -15,11 +16,11 @@ public class SingleNumber {
 
     //异或位运算
     public static int singleNumber1(int[] nums) {
-        int number = 0;
+        int numbers = 0;
 
         for (int num : nums) {
-            number ^= num;
+            numbers ^= num;
         }
-        return number;
+        return numbers;
     }
 }
